438d28ff99c851e2c1dd56929aa06a9e4c701292
Tomasz Jasiukiewicz <t.jasiukiewicz@google.com>
2020-02-24 12:18:00 +0100

    US1

--- board.c
+++ board.c
@@ -1,4 +1,6 @@
 #include "board.h"
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define BOARD_WIDTH  7
@@ -8,25 +10,47 @@ struct Board {
     struct Token * fields[BOARD_HEIGHT][BOARD_WIDTH];
 };
 
+inline static void show_column_names(void) {
+    for(unsigned i = 0; i < BOARD_WIDTH; ++i) {
+        printf(" %d", i + 1);
+    }
+    putchar('\n');
+}
+
+inline static void show_row_separator(void) {
+    for(unsigned i = 0; i < BOARD_WIDTH; ++i) {
+        printf("+-");
+    }
+    puts("+");
+}
+
+inline static void show_row(const struct Board * board, unsigned row) {
+    for(unsigned col = 0; col < BOARD_WIDTH; ++col) {
+        printf("|%c", board->fields[row][col] == NULL ? ' ': '?');
+    }
+    puts("|");
+}
+
 static struct Board board;
 
 struct Board * board_init(void) {
     // Inicjalizacja null
-    for(unsigned row = 0; row < BOARD_HEIGHT; ++row) {
-        for(unsigned col = 0; col < BOARD_WIDTH; ++col) {
-            board.fields[row][col] = NULL;
-        }
-    }
+    memset(board.fields, (int)NULL, BOARD_WIDTH * BOARD_HEIGHT * sizeof(struct Token *));
     return &board;
 }
 
 void board_print(const struct Board * board) {
-    // Wyświetlanie stanu board
-    (void) board;
+    show_column_names();
+    for(unsigned row = 0; row < BOARD_HEIGHT; ++row) {
+        show_row_separator();
+        show_row(board, row);
+    }
+    show_row_separator();
+    show_column_names();
 }
 
 bool board_insert_token(struct Board * board, const struct Token * token, short column) {
-    // Obsługa wrzucenia token'u
+    // TODO: Obsługa wrzucenia token'u
     return true;
 
     (void) board;
